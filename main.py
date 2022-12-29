5e3c2219b42d987c3d8f1cbe08ae89136f8cbc0f
Skocimis <aki.spremo@gmail.com>
2022-12-29 19:46:58 +0100

    Sequences stored in constant memory

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ mch = 5
 mss = 3
 gap = 9
 
-A = transformStr("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB") #columns
-B = transformStr("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB") #rows
+A = transformStr("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") #columns
+B = transformStr("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") #rows
 
 la = A.shape[1]
 lb = B.shape[1]
@@ -22,6 +22,8 @@ matrix = np.zeros((lb+1, la+1), np.int32)
 
 
 mod = SourceModule("""
+  __constant__  int A[8000];
+  __constant__  int B[8000];
   __global__ void arrayMax(int* array, int n, int* result){
     __shared__ int data[1024];
     int idx = threadIdx.x;
@@ -42,7 +44,7 @@ mod = SourceModule("""
     }
 
   }
-  __global__ void smithWaterman(int* matrix, int* A, int* B, int la, int lb, int match, int miss, int gap){
+  __global__ void smithWaterman(int* matrix, int la, int lb, int match, int miss, int gap){
     int min = (la<lb)?la:lb;
     int max = (la>lb)?la:lb;
     int k = 1;
@@ -70,13 +72,14 @@ mod = SourceModule("""
 
 matrix_gpu = cuda.mem_alloc(matrix.nbytes)
 cuda.memcpy_htod(matrix_gpu, matrix)
-A_gpu = cuda.mem_alloc(A.nbytes)
+
+A_gpu = mod.get_global("A")[0]
 cuda.memcpy_htod(A_gpu, A)
-B_gpu = cuda.mem_alloc(B.nbytes)
+B_gpu = mod.get_global("B")[0]
 cuda.memcpy_htod(B_gpu, B)
 
 funcSW = mod.get_function("smithWaterman")
-funcSW(matrix_gpu, A_gpu, B_gpu, np.int32(la), np.int32(lb), np.int32(mch), np.int32(mss), np.int32(gap), block = (min(la, lb), 1, 1), grid = (1, 1, 1))
+funcSW(matrix_gpu, np.int32(la), np.int32(lb), np.int32(mch), np.int32(mss), np.int32(gap), block = (min(la, lb), 1, 1), grid = (1, 1, 1))
 cuda.memcpy_dtoh(matrix, matrix_gpu)
 print("Matrix: \n", matrix)
 
@@ -94,4 +97,4 @@ while n!=1:
 cuda.memcpy_dtoh(result, result_gpu)
 result = result[0]
 
-print("Optimal alignment: ", result)
+print("Maximum: ", result)
